ede2ac51ab4167b76844b8478b6f7cd50a533623
Giovane <giovaneaalmeida27@gmail.com>
2021-10-22 21:56:37 -0300

    refact: move sut creation to a factory helper method

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -5,9 +5,13 @@ class TokenGenerator {
   }
 }
 
+const makeSut = () => {
+  return new TokenGenerator()
+}
+
 describe('Token Generator', () => {
   test('Should return null if JWT returns null', async () => {
-    const sut = new TokenGenerator()
+    const sut = makeSut()
     jwt.token = null
     const token = await sut.generate('any_id')
 
@@ -15,7 +19,7 @@ describe('Token Generator', () => {
   })
 
   test('Should return token if JWT returns a token', () => {
-    const sut = new TokenGenerator()
+    const sut = makeSut()
     const token = sut.generate('any_id')
     expect(token).toBe(jwt.token)
   })
