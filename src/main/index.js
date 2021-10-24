b1e1a205c22a075baf98b0174165f018741db931
Giovane <giovaneaalmeida27@gmail.com>
2021-10-24 08:35:59 -0300

    connect to mongodb before listen to app

--- src/main/index.js
+++ src/main/index.js
@@ -3,6 +3,12 @@
  * É o nosso Composition Root, onde devem ser criadas todas as instancias de nossas classes.
  * É onde inicializamos também a API. Irei instalar e configurar o Express e alguns middlewares.
 */
-const app = require('./config/app')
+const MongoHelper = require('../infra/helpers/mongo-helper')
+const env = require('./config/env')
 
-app.listen(5858, () => console.log('Server Running on port 5858'))
+MongoHelper.connect(env.mongoUrl)
+  .then(() => {
+    const app = require('./config/app')
+    app.listen(5858, () => console.log('Server Running on port 5858'))
+  })
+  .catch(console.error)
