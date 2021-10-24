b6f128938386c830cebb8cbf12e245e2a05c84fe
Giovane <giovaneaalmeida27@gmail.com>
2021-10-24 07:59:59 -0300

    index.js

--- src/main/index.js
+++ src/main/index.js
@@ -3,4 +3,6 @@
  * É o nosso Composition Root, onde devem ser criadas todas as instancias de nossas classes.
  * É onde inicializamos também a API. Irei instalar e configurar o Express e alguns middlewares.
 */
-// const app = require('./config/app')
+const app = require('./config/app')
+
+app.listen(5858, () => console.log('Server Running on port 5858'))
