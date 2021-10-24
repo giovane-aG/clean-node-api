07ebab38e389e391b7c9f8fd32613b72c2fdaa80
Giovane <giovaneaalmeida27@gmail.com>
2021-10-23 21:09:45 -0300

    disable x-powered-by header

---
+++ src/main/config/setup.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest')
+const app = require('./app')
+
+describe('App Setup', () => {
+  test('Shoud disable x-powered-by header', async () => {
+    app.get('/', (req, res) => {
+      res.send('')
+    })
+
+    const res = await request(app).get('/')
+    expect(res.headers['x-powered-by']).toBeUndefined()
+  })
+})
--- src/main/index.js
+++ src/main/index.js
@@ -3,8 +3,4 @@
  * É o nosso Composition Root, onde devem ser criadas todas as instancias de nossas classes.
  * É onde inicializamos também a API. Irei instalar e configurar o Express e alguns middlewares.
 */
-
-const express = require('express')
-const app = express()
-
-app.listen(5858, () => console.log('Server running'))
+// const app = require('./config/app')
