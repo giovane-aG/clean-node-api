4f688a159bd7f250665d2c6bf445eff1529fd435
Giovane <giovaneaalmeida27@gmail.com>
2021-10-26 06:49:36 -0300

    refact: rename getDeb with getCollection

--- src/infra/load-user-by-email-repository.js
+++ src/infra/load-user-by-email-repository.js
@@ -4,9 +4,9 @@ class LoadUserByEmailRepository {
   async load (email) {
     if (!email) throw new MissingParamError('email')
 
-    const db = await MongoHelper.getDb()
+    const userModel = await MongoHelper.getCollection('users')
 
-    const user = await db.collection('users').findOne({
+    const user = await userModel.findOne({
       email
     }, {
       projection: {
